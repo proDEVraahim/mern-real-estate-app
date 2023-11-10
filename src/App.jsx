183071356305b872408cca0d2e42eea4c50fd147
Raahim Arshad <raahimarshad21@gmail.com>
2023-11-10 19:38:11 +0500

    added pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "../src/pages/Home";
+import SignIn from "./pages/Signin"
+import SignUp from "./pages/Signup"
+import Profile from "./pages/Profile";
+import About from "./pages/About";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
